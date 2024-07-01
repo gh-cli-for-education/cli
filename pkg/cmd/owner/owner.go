711aa5ef7f5f9244df0adaa54c5045d4bb4740c9
DiegoPerezGarcia <alu0101345918@ull.edu.es>
2024-07-01 16:23:17 +0100

    base gh owner command

--- pkg/cmd/owner/owner.go
+++ pkg/cmd/owner/owner.go
@@ -1,40 +1,68 @@
 package owner
 
 import (
-	"github.com/MakeNowJust/heredoc"
+	"net/http"
 
+	"github.com/MakeNowJust/heredoc"
+	"github.com/cli/cli/v2/internal/gh"
 	"github.com/cli/cli/v2/pkg/cmdutil"
+	"github.com/cli/cli/v2/pkg/iostreams"
 	"github.com/spf13/cobra"
 )
 
+type OwnerOptions struct {
+	Config     func() (gh.Config, error)
+	IO         *iostreams.IOStreams
+	HttpClient func() (*http.Client, error)
+
+	Owner string
+}
+
 func NewCmdOwner(f *cmdutil.Factory) *cobra.Command {
+	opts := &OwnerOptions{
+		IO:         f.IOStreams,
+		Config:     f.Config,
+		HttpClient: f.HttpClient,
+	}
+
 	cmd := &cobra.Command{
-		Use:   "owner",
+		Use:   "owner [OWNER]",
 		Short: "Manage default owner for GitHub CLI commands",
 		Long: `The owner command allows you to manage the default owner
-for GitHub CLI commands.`,
+			for GitHub CLI commands.`,
 		Example: heredoc.Doc(`
-			$ gh owner set-default
-			$ gh owner view
+			$ gh owner
+			$ gh owner GITHUB_USERNAME
 		`),
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) > 1 {
+				return cmdutil.FlagErrorf("accepts at most 1 arg(s), received %d", len(args))
+			}
+
+			return nil
+		},
 		Annotations: map[string]string{
 			"help:arguments": heredoc.Doc(`
 				A owner can be supplied as an argument in any of the following formats:
 				- "OWNER"
 			`),
 		},
-		GroupID: "core",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if opts.Owner == "" {
+				// List default owner
+				// TODO: Implement list default owner
+				return nil
+			}
+
+			if opts.Owner != "" {
+				// Set default owner
+				// TODO: Implement set default owner
+				return nil
+			}
+
+			return nil
+		},
 	}
-
-	cmdutil.AddGroup(cmd, "General commands",
-		// ownerListCmd.NewCmdList(f, nil),
-		// ownerInfoCmd.NewCmdInfo(f, nil),
-	)
-
-	cmdutil.AddGroup(cmd, "Targeted commands",
-	  // ownerSetDefaultCmd.NewCmdSetDefault(f, nil),
-		// ownerReposCmd.NewCmdView(f, nil),
-	)
 
 	return cmd
 }
